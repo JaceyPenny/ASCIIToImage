50ce6f7efe45244679dd5f4bcdc65b0230ea6efd
Jace McPherson <jace-mcpherson@att.net>
2017-04-04 13:37:08 -0500

    Added support for turning image into ASCII art (the logical first step to a backwards conversion).

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.awt.image.Raster;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
@@ -17,34 +17,118 @@ public class Main {
     public static void main(String[] args) {
         setupLetterMap();
 
-        try {
-            BufferedReader fileReader = new BufferedReader(new FileReader(new File("input.txt")));
 
-            ArrayList<String> lines = new ArrayList<>();
-            String nextLine;
-            while ((nextLine = fileReader.readLine()) != null) {
-                lines.add(nextLine);
-            }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("1 - Image to ASCII\n2- ASCII to Image\n$ ");
+        int input = Integer.parseInt(scanner.nextLine());
 
-            int asciiHeight = lines.size();
-            int asciiWidth = lines.get(0).length();
+        System.out.print("Input file name: ");
+        String inputFileName = scanner.nextLine();
 
-            int imageHeight = asciiHeight * LETTER_HEIGHT;
-            int imageWidth = asciiWidth * LETTER_WIDTH;
+        System.out.print("Output file name: ");
+        String outputFileName = scanner.nextLine();
 
-            BufferedImage image = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB);
+        if (input == 1) {
+            try {
+                BufferedImage image = ImageIO.read(new File(inputFileName));
 
-            for (int i_y = 0; i_y < asciiHeight; i_y++) {
-                for (int i_x = 0; i_x < asciiWidth; i_x++) {
-                    char character = lines.get(i_y).charAt(i_x);
-                    fillCharacterAtPosition(image, character, i_x, i_y);
+                int lineLength = image.getWidth() / LETTER_WIDTH;
+                int lineCount = image.getHeight() / LETTER_HEIGHT;
+
+                System.out.println("Creating " + lineLength + " lines each with " + lineCount + " characters");
+
+                PrintWriter fileWriter = new PrintWriter(new FileOutputStream(new File(outputFileName)));
+
+                Raster raster = image.getData();
+
+                for (int y = 0; y < lineCount; y++) {
+                    StringBuilder thisLine = new StringBuilder();
+                    for (int x = 0; x < lineLength; x++) {
+                        char character = getCharForPosition(x, y, raster);
+                        thisLine.append(character);
+                    }
+                    fileWriter.println(thisLine.toString());
                 }
+
+                fileWriter.close();
+            } catch (IOException e) {
+
+            }
+        } else {
+            try {
+                BufferedReader fileReader = new BufferedReader(new FileReader(new File(inputFileName)));
+
+                ArrayList<String> lines = new ArrayList<>();
+                String nextLine;
+                while ((nextLine = fileReader.readLine()) != null) {
+                    lines.add(nextLine);
+                }
+
+                int asciiHeight = lines.size();
+                int asciiWidth = lines.get(0).length();
+
+                int imageHeight = asciiHeight * LETTER_HEIGHT;
+                int imageWidth = asciiWidth * LETTER_WIDTH;
+
+                BufferedImage image = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB);
+
+                for (int i_y = 0; i_y < asciiHeight; i_y++) {
+                    for (int i_x = 0; i_x < asciiWidth; i_x++) {
+                        char character = lines.get(i_y).charAt(i_x);
+                        fillCharacterAtPosition(image, character, i_x, i_y);
+                    }
+                }
+
+                ImageIO.write(image, "png", new File(outputFileName));
+
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
+    private static char getCharForPosition(int x, int y, Raster image) {
+
+        int grayscaleSum = 0;
+
+
+        for (int i_y = 0; i_y < LETTER_HEIGHT; i_y++) {
+            for (int i_x = 0; i_x < LETTER_WIDTH; i_x++) {
+                int[] pixelData = new int[4];
+                image.getPixel(x*LETTER_WIDTH + i_x, y*LETTER_HEIGHT + i_y, pixelData);
+                grayscaleSum += (pixelData[0] + pixelData[1] + pixelData[2]) / 3;
+            }
+        }
+
+        grayscaleSum /= LETTER_HEIGHT * LETTER_WIDTH;
+
+        char closestCharBelow = 'M';
+        int closestBelow = 0;
+
+        char closestCharAbove = ' ';
+        int closestAbove = 255;
+
+        for (Map.Entry<Character, Integer> entry : LETTER_MAP.entrySet()) {
+            char character = entry.getKey();
+            int value = entry.getValue();
+            if (value > grayscaleSum && value < closestAbove) {
+                closestAbove = value;
+                closestCharAbove = character;
             }
 
-            ImageIO.write(image, "png", new File("output.png"));
+            if (value < grayscaleSum && value > closestBelow) {
+                closestBelow = value;
+                closestCharBelow = character;
+            }
+        }
 
-        } catch (IOException e) {
+        int aboveDifference = closestAbove - grayscaleSum;
+        int belowDifference = grayscaleSum - closestBelow;
 
+        if (aboveDifference < belowDifference) {
+            return closestCharAbove;
+        } else {
+            return closestCharBelow;
         }
     }
 
